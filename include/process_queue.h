df460c45913933c8e674121884e734d6786402b1
Gabriel Souza <santos.gabriel@aluno.ufca.edu.br>
2026-08-12 00:37:17 -0300

    feat(process_queue): add insertion removal and queries

--- include/process_queue.h
+++ include/process_queue.h
@@ -1,5 +1,5 @@
-# ifndef PROCESS_QUEUE_H
-# define PROCESS_QUEUE_H
+#ifndef PROCESS_QUEUE_H
+#define PROCESS_QUEUE_H
 
 #include <stddef.h>
 #include "process.h"
@@ -7,6 +7,7 @@
 typedef struct ProcessQueue ProcessQueue;
 
 typedef int (*ProcessComparator)(const Process *p1, const Process *p2);
+typedef int (*QueueIterator)(Process *p, void *args);
 
 typedef struct {
     size_t capacity; /* capacidade máxima da fila */
@@ -16,8 +17,13 @@ typedef struct {
 ProcessQueue *queue_new(QueueAttr attr);
 void queue_destroy(ProcessQueue **q);
 
-void queue_insert(ProcessQueue *q, Process *p);
+size_t queue_size(ProcessQueue *q);
+Process *queue_at(ProcessQueue *q, size_t index); /* Retorna o processo na posição index da fila sem removê-lo */
+
+Process *queue_peek(ProcessQueue *q); /* Retorna o processo no início da fila sem removê-lo */
+void queue_insert(ProcessQueue *restrict q, Process *restrict p);
 Process *queue_remove(ProcessQueue *q);
-bool queue_is_empty(ProcessQueue *q);
+
+Process *queue_for_each(ProcessQueue *q, QueueIterator iter);
 
 #endif /* PROCESS_QUEUE_H */
